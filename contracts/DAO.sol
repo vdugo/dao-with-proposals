4916ca18bef9fd4f1f3e91808840c0502fafce00
Vincent <v3rdugo@yahoo.com>
2023-06-07 16:23:24 -0700

    proposals and tests

--- contracts/DAO.sol
+++ contracts/DAO.sol
@@ -10,6 +10,27 @@ contract DAO
     Token public token;
     uint256 public quorum;
 
+    struct Proposal
+    {
+        uint256 id;
+        string name;
+        uint256 amount;
+        address payable recipient;
+        uint256 votes;
+        bool finalized;
+    }
+
+    uint256 public proposalCount;
+    mapping(uint256 => Proposal) public proposals;
+
+    event Propose
+    (
+        uint256 id,
+        uint256 amount,
+        address recipient,
+        address creator
+    );
+
     constructor(Token _token, uint256 _quorum)
     {
         owner = msg.sender;
@@ -19,4 +40,32 @@ contract DAO
 
     // allow contract to receive Ether
     receive() external payable {}
+
+    // proposals
+    // Examples:
+    // 1. Send 100 ETH to Tom
+    // 2. Send 50 ETH to Jane for website development
+    // 3. Send 1 ETH to Julie marketing
+    function createProposal
+    (
+    string memory _name,
+    uint256 _amount,
+    address payable _recipient
+    ) external
+    {
+        proposalCount += 1;
+        // create a Proposal
+        proposals[proposalCount] = 
+        Proposal
+        (
+            proposalCount,
+            _name,
+            _amount,
+            _recipient,
+            0,
+            false
+        );
+
+        emit Propose(proposalCount, _amount, _recipient, msg.sender);
+    }
 }
--- test/DAO.js
+++ test/DAO.js
@@ -11,13 +11,15 @@ const ether = tokens
 describe('DAO', () => 
 {
     let token, dao
-    let deployer, funder
+    let deployer, funder, investor1, recipient
 
     beforeEach(async () => 
     {
         let accounts = await ethers.getSigners()
         deployer = accounts[0]
         funder = accounts[1]
+        investor1 = accounts[2]
+        recipient = accounts[3]
 
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Vin Coin', 'VIN', '1000000')
@@ -44,6 +46,45 @@ describe('DAO', () =>
         it('returns quorum', async () =>
         {
           expect(await dao.quorum()).to.equal('5000000000000000001')
+        })
+  
+    })
+
+    describe('Proposal Creation', () => 
+    {
+        let transaction, result
+
+        describe('Success', () =>
+        {
+            beforeEach(async () =>
+            {
+                transaction = await dao.connect(investor1).createProposal('proposal1', ether(100), recipient.address)
+                result = await transaction.wait()
+            })
+
+            it('updates the proposal count', async () =>
+            {
+                expect(await dao.proposalCount()).to.equal(1)
+                
+            })
+
+            it('updates proposals mapping', async () =>
+            {
+                const proposal = await dao.proposals(1)
+                expect(proposal.id).to.equal(1)
+                expect(proposal.amount).to.equal(ether(100))
+                expect(proposal.recipient).to.equal(recipient.address)
+            })
+
+            it('emits a Propose event', async () =>
+            {
+                await expect(transaction).to.emit(dao, 'Propose').withArgs(1, ether(100), recipient.address, investor1.address)
+            })
+        })
+
+        describe('Failure', () => 
+        {
+
         })
   
     })
